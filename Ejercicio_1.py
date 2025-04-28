a44709f8a4177f8f0a4e878391834d3aa10bab9b
Ema-cr <gaviriacardonaemanuel@gmail.com>
2025-04-28 13:35:02 -0500

    Add files via upload

---
+++ Ejercicio_1.py
@@ -0,0 +1,8 @@
+numero_int=int(input("Ingresa un numero: ")) 
+if numero_int <0:
+    print("Es un numero negativo")
+elif numero_int ==0:
+    print ("Es el numero 0")
+else:
+    print ("Es un numero positivo")
+
