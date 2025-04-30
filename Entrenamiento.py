4b2623c30a35d3ab6e1cc57434c07de3a0ad17ef
Ema-cr <gaviriacardonaemanuel@gmail.com>
2025-04-29 21:07:20 -0500

    Add files via upload

---
+++ Entrenamiento.py
@@ -0,0 +1,27 @@
+# Solicitar una calificacion
+# ingresar una lista de calificaciones y otro valor para comparar
+# mostrar si aprueba if elif else mostrar mensajes aprueba etc
+# un ciclo para recorrer las calificaciones y calcular promedio
+# un ciclo para contar calificaciones mayores al valor solicitado
+# evalua si una calificacion esta presente en la lista y cuenta cuantas veces aparece
+
+
+calificacion_soli=int(input("Ingresa tu calificacion entre 0 y 100: ")) #Solicitar una calificacion numerica
+if calificacion_soli >= 60:  #Evaluar si esta aprobada o reprobada
+    print("Aprobaste")
+else: print ("Reprobaste")
+
+califs= input("Ingresa tus calificaciones separadas por ','") #Pedir una lista de calificaciones
+valores= califs.split(",")         #Separar la cadena en elementos individuales
+numeros=[int(valor) for valor in valores]    #Convertimos cada valor a numeros int
+print("Lista de numeros: ", numeros)
+
+
+
+
+
+
+
+
+
+
